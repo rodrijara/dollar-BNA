edbcedadf902aac4434c5a1d0f1d34841181e477
rodrijara <rodrigoagustinjara@gmail.com>
2020-11-30 17:39:43 -0300

    Save dollar exchange rate into a SQLite DB

--- src/dollar_BNA_sqlite.py
+++ src/dollar_BNA_sqlite.py
@@ -2,26 +2,26 @@
 
 import requests
 from bs4 import BeautifulSoup
-from connections import create_connection, execute_query
+from connections_sqlite import create_connection, execute_query
 
 url = 'https://www.bna.com.ar'
-response = requests.get(url)
+source = requests.get(url).text
 
 # scrapping the url
-soup = BeautifulSoup(response.text, 'lxml')
+soup = BeautifulSoup(source, 'lxml')
 pane = soup.find('div', class_='tab-pane fade in active')
 rates = pane.table.tbody.text
 date = pane.table.thead.text
 
 # get rates values
-_, _, usd_c, usd_v, _, eur_c, eur_v, *others = rates.split()
+_, _, usd_c, usd_v, *others = rates.split()
 date= date.split()[0]
 
 # stablish a SQLite connection and save the data
 
 print('... Connecting DB')
 
-connection = create_connection('./ratesDB')
+connection = create_connection('./BNA-dollar-DB')
 
 print('... Creating new table')
 
@@ -30,9 +30,7 @@ CREATE TABLE rates (
 id INTEGER PRIMARY KEY AUTOINCREMENT,
 date TEXT NOT NULL UNIQUE,
 usd_c REAL,
-usd_v REAL,
-eur_c REAL, 
-eur_v REAL
+usd_v REAL
 );
 """
 execute_query(connection, create_rates_table)
@@ -41,7 +39,7 @@ print('... Saving todays\' date and values in DB')
 
 save_rates = """
 INSERT INTO 
-rates (date, usd_c, usd_v, eur_c, eur_v)
+rates (date, usd_c, usd_v)
 VALUES
 (?, ?, ?, ?, ?)"""
-execute_query(connection, save_rates, date, usd_c, usd_v, eur_c, eur_v)
+execute_query(connection, save_rates, date, usd_c, usd_v)
