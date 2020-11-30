78f96848adb2c318fb305139938dc86030f8d79b
rodrijara <rodrigoagustinjara@gmail.com>
2020-11-30 12:01:15 -0300

    Script can get market rates

---
+++ env/lib/xml2Conf.sh
@@ -0,0 +1,8 @@
+#
+# Configuration file for using the XML library in GNOME applications
+#
+XML2_LIBDIR="-L/Users/rjara/devprojects/BNA-market-rates/env/lib"
+XML2_LIBS="-lxml2 -L/Users/rjara/devprojects/BNA-market-rates/env/lib -lz -L/Users/rjara/devprojects/BNA-market-rates/env/lib -llzma -lpthread -L/Users/rjara/devprojects/BNA-market-rates/env/lib -liconv -L/Users/rjara/devprojects/BNA-market-rates/env/lib -licui18n -licuuc -licudata -lm "
+XML2_INCLUDEDIR="-I/Users/rjara/devprojects/BNA-market-rates/env/include/libxml2 -I/Users/rjara/devprojects/BNA-market-rates/env/include"
+MODULE_VERSION="xml2-2.9.10"
+
---
+++ env/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/Users/rjara/devprojects/BNA-market-rates/env/lib"
+XSLT_LIBS="-lxslt -L/Users/rjara/devprojects/BNA-market-rates/env/lib -lxml2 -L/Users/rjara/devprojects/BNA-market-rates/env/lib -lz -L/Users/rjara/devprojects/BNA-market-rates/env/lib -llzma -lpthread -L/Users/rjara/devprojects/BNA-market-rates/env/lib -liconv -L/Users/rjara/devprojects/BNA-market-rates/env/lib -licui18n -licuuc -licudata -lm  "
+XSLT_INCLUDEDIR="-I/Users/rjara/devprojects/BNA-market-rates/env/include"
+MODULE_VERSION="xslt-1.1.33"
